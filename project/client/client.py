477279ec4a84afad76ad86700dc0faba0e8490fb
Mai Hoàng Đức <maihoangduc1103.ptts@gmail.com>
2023-12-26 15:11:17 +0700

    update26_12_2023_client-server_message

--- project/client/client.py
+++ project/client/client.py
@@ -2,22 +2,52 @@ import socket
 
 MAXLINE = 4096
 SERV_PORT = 3000
-
+host = "127.0.0.1" #default for local test
 def main():
-    host = input("Enter the server's IP address: ")
+    menu ="1. Login\n2. Register\n3. Exit"
+    menu1 = "1. Search Flights\n2. Book tickets\n3. Manage booked tickets\n4. Exit\n "
+
+    #host = input("Enter the server's IP address: ")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         try:
             client_socket.connect((host, SERV_PORT))
             print("Connected to the server. Type 'exit' to terminate.")
+            show_menu = True
             
-            while True:
-                message = client_socket.recv(MAXLINE).decode('utf-8')
-                if message.lower() == 'exit':
-                    break
-                print(message)
-                choice = input("Your message: ")
+            while True: 
+                if show_menu:
+                    print(menu)
+                
+                choice = input("Your message: ").lower()
                 client_socket.send(choice.encode())
+                
+                if choice == "exit":
+                    break
+                
+                message = client_socket.recv(MAXLINE).decode('utf-8')
+                
+                if message == "N_format":
+                    print("Invalid format. Please choose a valid option!")
+                    show_menu = True  
+                elif message == "Y_login":
+                    print("You've logged in successfully!\n")
+                    print(menu1)
+                    show_menu = False  
+                elif message == "N_login":
+                    print("Please check your username and password!\n")
+                    show_menu= True
+                elif message == "Y_register":
+                    print("You've register successfully!\n")
+                    print(menu1)
+                    show_menu = False 
+                elif message== "N_register":
+                    print("Your username has aleady existed!\n")     
+                    show_menu= True             
+                elif message=="N_in":
+                    print("Invalid format!")
+                    print(menu1)
+                    show_menu = False
         except Exception as e:
             print(f"Error connecting to the server: {e}")
             return
--- project/server/server.py
+++ project/server/server.py
@@ -20,11 +20,9 @@ def load_user_data():
         user_data = {}
     return user_data
 
-
 def save_user_data(user_data):
     with open(path, 'w') as file:
         json.dump(user_data, file)
-
 
 def load_data(filename):
     tmp_flight_data = []
@@ -34,44 +32,34 @@ def load_data(filename):
             flight.add_flight(tmp_flight_data, *data)
     return tmp_flight_data
 
-
 def handle_login(client_socket, user_data, username, password, tmp_flight_data):
     if username in user_data and user_data[username] == password:
         print("Login successful")
-        client_socket.send(
-            "Login successful, press enter for continuing\n".encode('utf-8'))
+        client_socket.send("Y_login".encode('utf-8'))
         you_are_in(client_socket, tmp_flight_data)
     else:
         print("Login failed")
-        client_socket.send("Login failed".encode('utf-8'))
-
+        client_socket.send("N_login".encode('utf-8'))
 
 def handle_registration(client_socket, user_data, username, password, tmp_flight_data):
     if username in user_data:
-        client_socket.send(
-            "Username already exists. Please choose another.".encode('utf-8'))
+        print("Username existed")
+        client_socket.send("N_register".encode('utf-8'))
     else:
         user_data[username] = password
         save_user_data(user_data)
-        client_socket.send("Registration successful".encode('utf-8'))
+        client_socket.send("Y_register".encode('utf-8'))
         you_are_in(client_socket, tmp_flight_data)
-
 
 def you_are_in(client_socket, tmp_flight_data):
     while True:
-        menu = "1. Search Flights\n2. Book tickets\n3. Manage booked tickets\n4. Exit\n "
-        client_socket.send(menu.encode('utf-8'))
-        print(menu)
-
         received1 = client_socket.recv(BUFFER_SIZE).decode('utf-8').strip()
         if received1.lower() == "exit":
             break
-
         type1 = received1.split(' ')
-        if len(type1) < 2 or type1[0].lower() not in ("login", "register"):
-            error_message = "Invalid format. Please try again!"
-            print(error_message)
-            client_socket.send(error_message.encode('utf-8'))
+        if len(type1) < 2 or type1[0].lower() not in ("search", "book", "manage"):
+            print("Invalid format")
+            client_socket.send("N_in".encode('utf-8'))
             continue
 
         if type1[0].lower() == "search":
@@ -147,8 +135,6 @@ def handle_client(client_socket, user_data, tmp_flight_data):
 
     while True:
         try:
-            menu = "1. Login\n2. Register\n3. Exit\n"
-            client_socket.send(menu.encode('utf-8'))
             received = client_socket.recv(BUFFER_SIZE).decode('utf-8').strip()
             print(f"Received login information: {received}")
 
@@ -158,10 +144,8 @@ def handle_client(client_socket, user_data, tmp_flight_data):
             type = received.split(' ')
 
             if len(type) != 2 or type[0].lower() not in ("login", "register"):
-                print(
-                    "Invalid login format. Please use: login <username>,<password> or register <username>,<password>")
-                client_socket.send(
-                    "Invalid login format. Please use: login <username>,<password>".encode('utf-8'))
+                print("Invalid format")
+                client_socket.send("N_format".encode('utf-8'))
                 continue
 
             username, password = type[1].split(',')
@@ -170,17 +154,16 @@ def handle_client(client_socket, user_data, tmp_flight_data):
 
             if type[0].lower() == "login":
                 print("Login requested")
-                handle_login(client_socket, user_data, username,
-                             password, tmp_flight_data)
+                handle_login(client_socket, user_data, username, password, tmp_flight_data)
             elif type[0].lower() == "register":
                 print("Registration requested")
-                handle_registration(client_socket, user_data,
-                                    username, password, tmp_flight_data)
+                handle_registration(client_socket, user_data, username, password, tmp_flight_data)
         except ConnectionResetError:
             break
 
     print(f"Connection closed by {client_socket.getpeername()}")
     client_socket.close()
+
 
 
 def main():
